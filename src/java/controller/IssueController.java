84c284c1257caedea36d753e8b332614ebffcbb5
HuyenPTN <huyenptn182769@gmail.com>
2024-11-26 13:26:18 +0700

    Fix bugs

--- src/java/controller/IssueController.java
+++ src/java/controller/IssueController.java
@@ -6,7 +6,6 @@ package controller;
 
 import jakarta.servlet.RequestDispatcher;
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
